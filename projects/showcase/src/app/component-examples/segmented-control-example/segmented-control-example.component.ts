c6c56018376a569f532b74ddb931d3fd36372e8d
Darwin Grisales <darwingrisalesg@gmail.com>
2022-04-21 21:17:48 -0500

    Fix route and add array options

--- projects/showcase/src/app/component-examples/segmented-control-example/segmented-control-example.component.ts
+++ projects/showcase/src/app/component-examples/segmented-control-example/segmented-control-example.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AclSegmentedColors, AclSegmentedSizes } from 'angular-components-library/segmented-control';
 
 @Component({
   selector: 'app-segmented-control-example',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./segmented-control-example.component.scss']
 })
 export class SegmentedControlExampleComponent implements OnInit {
+  nameAttr: string = "gato";
+  identifier: string = "element"
+
+  colors: AclSegmentedColors[] = [
+    'primary',
+    'secondary',
+    'tertiary',
+    'warning',
+    'alert',
+    'danger',
+    'info',
+    'notification',
+  ];
+
+  sizes: AclSegmentedSizes[] = ['small', 'medium', 'large'];
 
   constructor() { }
 
