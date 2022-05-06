5cec22b8500b8dbc2097a12c5e7481edeb61d8bd
Darwin Grisales <darwingrisalesg@gmail.com>
2022-05-05 23:53:51 -0500

    Refactor the id examples
    this commit eliminate the arrays of the examples and make it dynamic

--- projects/showcase/src/app/component-examples/segmented-control-example/segmented-control-example.component.ts
+++ projects/showcase/src/app/component-examples/segmented-control-example/segmented-control-example.component.ts
@@ -7,39 +7,6 @@ import { AclSegmentedColors, AclSegmentedSizes } from 'angular-components-librar
   styleUrls: ['./segmented-control-example.component.scss']
 })
 export class SegmentedControlExampleComponent {
-  smallDogs: string[] = [
-    'perro1',
-    'perro2',
-    'perro3',
-    'perro4',
-    'perro5',
-    'perro6',
-    'perro7',
-    'perro8,'
-  ]
-
-  mediumBunnies: string[] = [
-    'conejo1',
-    'conejo2',
-    'conejo3',
-    'conejo4',
-    'conejo5',
-    'conejo6',
-    'conejo7',
-    'conejo8,'
-  ]
-
-  largeCats: string[] = [
-    'gato1',
-    'gato2',
-    'gato3',
-    'gato4',
-    'gato5',
-    'gato6',
-    'gato7',
-    'gato8,'
-  ]
-
   colors: AclSegmentedColors[] = [
     'primary',
     'secondary',
